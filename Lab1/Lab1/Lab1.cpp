0545b241f2a7bbf6029479b7e583a5a83d6a0961
Михаил Потёмкин <michael.potyomkin@gmail.com>
2020-02-29 10:09:48 +0200

    Base difficulty done.

--- Lab1/Lab1/Lab1.cpp
+++ Lab1/Lab1/Lab1.cpp
@@ -4,15 +4,15 @@
 #include <string>
 
 struct Computers {
-	//string name;
+	char name[8];
 	float frequency;
 	int RAM;
 	bool dvdrom;
-	float price;
+	int price;
 };
 
 using namespace std;
-int main()
+int BaseDifficulty()
 {
 	srand(time(NULL));
 
@@ -21,11 +21,23 @@ int main()
 
 	for (int item = 0; item < N; item++)
 	{
-		//Warehouse[item].name = 
-		Warehouse[item].frequency = (float)(rand() % 30 + 20) / 10;
-		
+		/*Enter 2 random letter in name, then '-', then random 4 digits.*/
 
-		switch (rand() % 6 + 1)
+		for (int i = 0; i < 8; i++)
+		{
+			Warehouse[item].name[i] = (i > 2 ? 48 + rand() % 10 : (char)65 + rand() % 25);
+			if (i == 2) { Warehouse[item].name[i] = (char)45; }
+			/*last char in array must be nullvalue, otherwise trash will be shown in output*/
+			if (i == 7) { Warehouse[item].name[i] = NULL; }
+		}
+		
+		/*Randomly enter CPU freq in range [2,0 4,5] GHz*/
+
+		Warehouse[item].frequency = (float)(rand() % 25 + 20) / 10;
+
+		/*Randomly choose RAM from the pool*/
+
+		switch (rand() % 7 + 1)
 		{
 		case 1:Warehouse[item].RAM = 2; break;
 		case 2:Warehouse[item].RAM = 4; break;
@@ -37,12 +49,33 @@ int main()
 		default: Warehouse[item].RAM = 1;
 		}
 
+		/*Decide if the computer has DVD-ROM*/
+
 		Warehouse[item].dvdrom = rand() % 2;
 		
+		/*Calculate price of the computer based on previous values.*/
+		Warehouse[item].price = (int)Warehouse[item].dvdrom * 250 + Warehouse[item].frequency * 5000 + Warehouse[item].RAM * 125;
 		
-		cout << "Frequency: " << Warehouse[item].frequency << endl;
-		cout << "RAM: " << Warehouse[item].RAM << endl;
+		
+		cout << "Name: " << Warehouse[item].name << endl;
+		cout << "CPU Frequency: " << Warehouse[item].frequency << " GHz" << endl;
+		cout << "RAM: " << Warehouse[item].RAM << " GB" << endl;
 		cout << "Has a DVD-ROM: " << (Warehouse[item].dvdrom == true ? "Yes\n" : "No\n");
+		cout << "Price: " << Warehouse[item].price << " UAH" << endl;
+		cout << "\n\n";
 	}
 
+	cout << "All computers with RAM > 10 in Warehouse: \n\n";
+
+	for (int item = 0; item < 7; item++)
+	{
+		if (Warehouse[item].RAM > 10) {
+			cout << "Name: " << Warehouse[item].name << endl;
+			cout << "CPU Frequency: " << Warehouse[item].frequency << " GHz" << endl;
+			cout << "RAM: " << Warehouse[item].RAM << " GB" << endl;
+			cout << "Has a DVD-ROM: " << (Warehouse[item].dvdrom == true ? "Yes\n" : "No\n");
+			cout << "Price: " << Warehouse[item].price << " UAH" << endl;
+			cout << "\n\n";
+		}
+	}
 }
