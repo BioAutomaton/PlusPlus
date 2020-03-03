677f7ad023d1af52fa50e29a2a378606078bc372
Михаил Потёмкин <michael.potyomkin@gmail.com>
2020-03-03 19:58:23 +0200

    Minor changes & captions added

--- StrongPassword/StrongPassword/StrongPassword.cpp
+++ StrongPassword/StrongPassword/StrongPassword.cpp
@@ -7,34 +7,37 @@ using namespace std;
 
 bool strongPassword(char* password, bool* errors)
 {
-
+	/*The password is good by default. To make a bad password at least one error must be present.*/
 	bool result = true;
+
+	/*Length check*/
 	if (strlen(password) >= 8) { errors[0] = false; }
 	else { errors[0] = true; }
 
 	for (int i = 0; i < strlen(password); i++)
 	{
+		/*Lowercase check*/
 		if (islower(password[i]))
 		{
 			errors[1] = false;
 		}
-
+		/*Uppercase check*/
 		if (isupper(password[i]))
 		{
 			errors[2] = false;
 		}
-
+		/*Digit check*/
 		if (isdigit(password[i]))
 		{
 			errors[3] = false;
 		}
-
+		/*Special symbols check*/
 		if (ispunct(password[i]))
 		{
 			errors[4] = false;
 		}
 	}
-
+	/*If all errors are false, the password remains good. If at least one is true, the password becomes bad.*/
 	if (errors[0] || errors[1] || errors[2] || errors[3] || errors[4]) { result = false; }
 
 	return result;
@@ -44,15 +47,19 @@ int main()
 {
 	cout << "Password creation. The password must contain:\n-More than 8 characters\n-At least one lowercase letter\n-At least one uppercase letter";
 	cout << "\n-At least one digit\n-At least one special symbol\n";
-	char password[20];
+	char password[100];
 	cout << "You password: ";
-	bool errors[5] = { 1,1,1,1,1 };
+
+	/*By default all errors are true. By checking the entering programm will eliminate them if the password satisfies corresponding reqirement*/
+	bool errors[5] = { 1,1,1,1,1 };  
+
 	cin.ignore(cin.rdbuf()->in_avail());
-	cin.getline(password, 20);
-	bool isStrong = strongPassword(password, errors);
-	if (isStrong) { cout << "Good password.\n"; }
+	cin.getline(password, 100);
+
+	if (strongPassword(password, errors)) { cout << "Good password. Well done!\n"; }
 	else
 	{
+		/*Errors output*/
 		cout << "Bad password.\n";
 		cout << (errors[0] == true ? "\nThe password must contain more than 8 characters." : "\nLength is good.") << endl;
 		cout << (errors[1] == true ? "\nThe password must contain at least one lowercase letter." : "\nHas a lowercase letter.") << endl;
