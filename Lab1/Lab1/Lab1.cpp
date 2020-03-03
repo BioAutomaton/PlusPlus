40838883f12c1039aed657ba7d177e9df013bd8a
Михаил Потёмкин <michael.potyomkin@gmail.com>
2020-03-03 19:46:59 +0200

    Added a password-checking function.

--- Lab1/Lab1/Lab1.cpp
+++ Lab1/Lab1/Lab1.cpp
@@ -3,7 +3,9 @@
 #include <time.h>       
 #include <string>
 
-struct Computers {
+using namespace std;
+
+struct Computer {
 	char name[8];
 	float frequency;
 	int RAM;
@@ -11,13 +13,84 @@ struct Computers {
 	int price;
 };
 
-using namespace std;
-int BaseDifficulty()
-{
-	srand(time(NULL));
 
+struct Date {
+	short day;
+	short month;
+	short year;
+	bool isCorrect();
+};
+
+bool Date::isCorrect()
+{
+	bool result = false;
+	switch (month)
+	{
+	case 1:
+	case 3:
+	case 5:
+	case 7:
+	case 8:
+	case 10:
+	case 12:
+	{
+		if ((day <= 31) && (day > 0))
+			result = true;
+		break;
+	}
+
+	case 4:
+	case 6:
+	case 9:
+	case 11:
+	{
+		if ((day <= 30) && (day > 0))
+			result = true;
+		break;
+	}
+
+	case 2:
+	{
+		if (year % 4 != 0)
+		{
+			if ((day <= 28) && (day > 0))
+				result = true;
+		}
+		else
+			if (year % 400 == 0)
+			{
+				if ((day <= 29) && (day > 0))
+					result = true;
+			}
+			else
+				if ((year % 100 == 0) && (year % 400 != 0))
+				{
+					if ((day == 28) && (day > 0))
+						result = true;
+				}
+				else
+					if ((year % 4 == 0) && (year % 100 != 0))
+						if ((day <= 29) && (day > 0))
+							result = true;
+		break;
+	}
+
+	default:
+		result = false;
+	}
+
+	if (year == 2020 && month > 3) { result = false; }
+	if (year > 2020) { result = false; }
+
+	return result;
+}
+
+
+
+void BaseLevel()
+{
 	const int N = 10;
-	Computers Warehouse[N];
+	Computer Warehouse[N];
 
 	for (int item = 0; item < N; item++)
 	{
@@ -30,7 +103,7 @@ int BaseDifficulty()
 			/*last char in array must be nullvalue, otherwise trash will be shown in output*/
 			if (i == 7) { Warehouse[item].name[i] = NULL; }
 		}
-		
+
 		/*Randomly enter CPU freq in range [2,0 4,5] GHz*/
 
 		Warehouse[item].frequency = (float)(rand() % 25 + 20) / 10;
@@ -52,11 +125,11 @@ int BaseDifficulty()
 		/*Decide if the computer has DVD-ROM*/
 
 		Warehouse[item].dvdrom = rand() % 2;
-		
+
 		/*Calculate price of the computer based on previous values.*/
 		Warehouse[item].price = (int)Warehouse[item].dvdrom * 250 + Warehouse[item].frequency * 5000 + Warehouse[item].RAM * 125;
-		
-		
+
+
 		cout << "Name: " << Warehouse[item].name << endl;
 		cout << "CPU Frequency: " << Warehouse[item].frequency << " GHz" << endl;
 		cout << "RAM: " << Warehouse[item].RAM << " GB" << endl;
@@ -78,4 +151,110 @@ int BaseDifficulty()
 			cout << "\n\n";
 		}
 	}
+}
+
+void MidLevel()
+{
+	struct Auto
+	{
+		string mark;
+		string dealer;
+		string type;
+		Date manufacture;
+		Date registration;
+	};
+
+
+	const int N = 1000;
+	Auto Parking[N];
+
+	for (int car = 0; car < N; car++)
+	{
+		/*Car type*/
+
+		switch (rand() % 5)
+		{
+		case 0:Parking[car].type = "Sedan";		break;
+		case 1:Parking[car].type = "Pickup";	break;
+		case 2:Parking[car].type = "Hatchback";	break;
+		case 3:Parking[car].type = "Universal";	break;
+		case 4:Parking[car].type = "Coupe";		break;
+		default: Parking[car].type = "N/A";		break;
+		}
+
+		/*Car mark + dealer. I gather that these parameters should be paired, cause they usually are.*/
+
+		switch (rand() % 10)
+		{
+		case 0: Parking[car].mark = "Audi"; Parking[car].dealer = "Germany";		break;
+		case 1: Parking[car].mark = "Tesla"; Parking[car].dealer = "USA";			break;
+		case 2: Parking[car].mark = "Nissan"; Parking[car].dealer = "Japan";		break;
+		case 3: Parking[car].mark = "BMW"; Parking[car].dealer = "Germany";			break;
+		case 4: Parking[car].mark = "Chevrolet"; Parking[car].dealer = "France";	break;
+		case 5: Parking[car].mark = "Mercedes"; Parking[car].dealer = "Germany";	break;
+		case 6: Parking[car].mark = "Jaguar"; Parking[car].dealer = "UK";			break;
+		case 7: Parking[car].mark = "Porsche"; Parking[car].dealer = "Germany";		break;
+		case 8: Parking[car].mark = "Hyundai"; Parking[car].dealer = "South Korea";	break;
+		case 9: Parking[car].mark = "Honda"; Parking[car].dealer = "Japan";			break;
+		default: Parking[car].mark = "N/A"; Parking[car].dealer = "N/A";			break;
+		}
+
+		/*Date of manufacture */
+		do
+		{
+			Parking[car].manufacture.day = rand() % 31 + 1;
+			Parking[car].manufacture.month = rand() % 12 + 1;
+			Parking[car].manufacture.year = rand() % 10 + 2000;
+		} while (!Parking[car].manufacture.isCorrect());
+
+		/*Date of registration based of manufacture date*/
+		do
+		{
+			Parking[car].registration.day = Parking[car].manufacture.day + rand() % 29 - rand() % 29;
+			Parking[car].registration.month = Parking[car].manufacture.month + rand() % 12 - rand() % 12;
+			Parking[car].registration.year = Parking[car].manufacture.year + rand() % 30;
+		} while (!Parking[car].registration.isCorrect());
+	}
+
+	cout << "Cars that have been manufactured before 2005 and registered this year: \n\n";
+
+	for (int car = 0; car < N; car++)
+	{
+		if (Parking[car].registration.year == 2020 && Parking[car].manufacture.year < 2005)
+		{
+			cout << Parking[car].mark << "\n";
+			cout << Parking[car].dealer << "\n";
+			cout << Parking[car].type << "\n";
+			cout << Parking[car].manufacture.day << "." << Parking[car].manufacture.month << "." << Parking[car].manufacture.year << " MAN" << endl;
+			cout << Parking[car].registration.day << "." << Parking[car].registration.month << "." << Parking[car].registration.year << " REG" << endl;
+			cout << "\n\n";
+		}
+
+	}
+
+}
+
+void HighLevel()
+{
+
+}
+
+int main()
+{
+	int select = 0;
+	do {
+		srand(time(NULL));
+		cout << "Variant 22. Lab 1.\nSelect difficulty level\n";
+		cout << "1 - Base level\n2 - Mid level \n3 - High level\n";
+		cout << "Or enter 0 to exit.\n";
+		cin >> select;
+		switch (select)
+		{
+		case 0: break;
+		case 1:BaseLevel(); break;
+		case 2:MidLevel(); break;
+		case 3:HighLevel(); break;
+		default: cout << "Wrong entering."; break;
+		}
+	} while (select != 0);
 }
---
+++ StrongPassword/StrongPassword/StrongPassword.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <string.h>
+#include <string>
+#include <ctype.h>
+
+using namespace std;
+
+bool strongPassword(char* password, bool* errors)
+{
+
+	bool result = true;
+	if (strlen(password) >= 8) { errors[0] = false; }
+	else { errors[0] = true; }
+
+	for (int i = 0; i < strlen(password); i++)
+	{
+		if (islower(password[i]))
+		{
+			errors[1] = false;
+		}
+
+		if (isupper(password[i]))
+		{
+			errors[2] = false;
+		}
+
+		if (isdigit(password[i]))
+		{
+			errors[3] = false;
+		}
+
+		if (ispunct(password[i]))
+		{
+			errors[4] = false;
+		}
+	}
+
+	if (errors[0] || errors[1] || errors[2] || errors[3] || errors[4]) { result = false; }
+
+	return result;
+}
+
+int main()
+{
+	cout << "Password creation. The password must contain:\n-More than 8 characters\n-At least one lowercase letter\n-At least one uppercase letter";
+	cout << "\n-At least one digit\n-At least one special symbol\n";
+	char password[20];
+	cout << "You password: ";
+	bool errors[5] = { 1,1,1,1,1 };
+	cin.ignore(cin.rdbuf()->in_avail());
+	cin.getline(password, 20);
+	bool isStrong = strongPassword(password, errors);
+	if (isStrong) { cout << "Good password.\n"; }
+	else
+	{
+		cout << "Bad password.\n";
+		cout << (errors[0] == true ? "\nThe password must contain more than 8 characters." : "\nLength is good.") << endl;
+		cout << (errors[1] == true ? "\nThe password must contain at least one lowercase letter." : "\nHas a lowercase letter.") << endl;
+		cout << (errors[2] == true ? "\nThe password must contain at least one uppercase letter." : "\nHas an uppercase letter.") << endl;
+		cout << (errors[3] == true ? "\nThe password must contain at least one digit." : "\nHas a digit.") << endl;
+		cout << (errors[4] == true ? "\nThe password must contain at least one special symbol." : "\nHas a special symbol.") << endl;
+	}
+}
+
+
