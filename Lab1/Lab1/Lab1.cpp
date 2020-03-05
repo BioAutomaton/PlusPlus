dbe62defdea23ce5e783a00becf06381eda73367
Михаил Потёмкин <michael.potyomkin@gmail.com>
2020-03-05 18:25:34 +0200

    High level done + captions added.

--- Lab1/Lab1/Lab1.cpp
+++ Lab1/Lab1/Lab1.cpp
@@ -2,6 +2,8 @@
 #include <stdlib.h>     
 #include <time.h>       
 #include <string>
+#include <string.h>
+#include <ctype.h>
 
 using namespace std;
 
@@ -215,7 +217,8 @@ void MidLevel()
 			Parking[car].registration.year = Parking[car].manufacture.year + rand() % 30;
 		} while (!Parking[car].registration.isCorrect());
 	}
-
+	
+	/*Output*/
 	cout << "Cars that have been manufactured before 2005 and registered this year: \n\n";
 
 	for (int car = 0; car < N; car++)
@@ -225,8 +228,8 @@ void MidLevel()
 			cout << Parking[car].mark << "\n";
 			cout << Parking[car].dealer << "\n";
 			cout << Parking[car].type << "\n";
-			cout << Parking[car].manufacture.day << "." << Parking[car].manufacture.month << "." << Parking[car].manufacture.year << " MAN" << endl;
-			cout << Parking[car].registration.day << "." << Parking[car].registration.month << "." << Parking[car].registration.year << " REG" << endl;
+			cout << Parking[car].manufacture.day << "." << Parking[car].manufacture.month << "." << Parking[car].manufacture.year << " Manufacture date" << endl;
+			cout << Parking[car].registration.day << "." << Parking[car].registration.month << "." << Parking[car].registration.year << " Registration date" << endl;
 			cout << "\n\n";
 		}
 
@@ -236,7 +239,137 @@ void MidLevel()
 
 void HighLevel()
 {
+	struct ZNAK
+	{
+		string name;
+		string zodiac;
+		Date birthday;
+	};
 
+	const int N = 10;
+	ZNAK Crowd[N];
+
+	for (int person = 0; person < N; person++)
+	{
+		/*First name generator*/
+		switch (rand() % 10)
+		{
+		case 0: Crowd[person].name = "Matthew"; break;
+		case 1: Crowd[person].name = "Sam"; break;
+		case 2: Crowd[person].name = "Laura"; break;
+		case 3: Crowd[person].name = "Taliesin"; break;
+		case 4: Crowd[person].name = "Ashley"; break;
+		case 5: Crowd[person].name = "Travis"; break;
+		case 6: Crowd[person].name = "Marisha"; break;
+		case 7: Crowd[person].name = "Liam"; break;
+		case 8: Crowd[person].name = "Brian"; break;
+		case 9: Crowd[person].name = "Felicia"; break;
+		default:Crowd[person].name = "John"; break;
+		}
+		/*Third name generator*/
+		switch (rand() % 10)
+		{
+		case 0: Crowd[person].name += " Mercer"; break;
+		case 1: Crowd[person].name += " Riegel"; break;
+		case 2: Crowd[person].name += " Bailey"; break;
+		case 3: Crowd[person].name += " Jaffe"; break;
+		case 4: Crowd[person].name += " Johnson"; break;
+		case 5: Crowd[person].name += " Willingham"; break;
+		case 6: Crowd[person].name += " Ray"; break;
+		case 7: Crowd[person].name += " O'Brien"; break;
+		case 8: Crowd[person].name += " Foster"; break;
+		case 9: Crowd[person].name += " Day"; break;
+		default:Crowd[person].name += " Doe"; break;
+		}
+		/*Birthday generator*/
+		do
+		{
+			Crowd[person].birthday.day = rand() % 31 + 1;
+			Crowd[person].birthday.month = rand() % 12 + 1;
+			Crowd[person].birthday.year = rand() % 70 + 1950;
+		} while (!Crowd[person].birthday.isCorrect());
+		/*Zodiac sign determiner*/
+		switch (Crowd[person].birthday.month)
+		{
+		case 1: Crowd[person].zodiac = (Crowd[person].birthday.day >= 20 ? "Aquarius" : "Capricorn"); break;
+		case 2: Crowd[person].zodiac = (Crowd[person].birthday.day >= 19 ? "Pisces" : "Aquarius"); break;
+		case 3: Crowd[person].zodiac = (Crowd[person].birthday.day >= 21 ? "Aries" : "Pisces"); break;
+		case 4: Crowd[person].zodiac = (Crowd[person].birthday.day >= 20 ? "Taurus" : "Aries"); break;
+		case 5: Crowd[person].zodiac = (Crowd[person].birthday.day >= 21 ? "Gemini" : "Taurus"); break;
+		case 6: Crowd[person].zodiac = (Crowd[person].birthday.day >= 21 ? "Cancer" : "Gemini"); break;
+		case 7: Crowd[person].zodiac = (Crowd[person].birthday.day >= 23 ? "Leo" : "Cancer"); break;
+		case 8: Crowd[person].zodiac = (Crowd[person].birthday.day >= 23 ? "Virgo" : "Leo"); break;
+		case 9: Crowd[person].zodiac = (Crowd[person].birthday.day >= 23 ? "Libra" : "Virgo"); break;
+		case 10: Crowd[person].zodiac = (Crowd[person].birthday.day >= 23 ? "Scorpio" : "Libra"); break;
+		case 11: Crowd[person].zodiac = (Crowd[person].birthday.day >= 22 ? "Sagittarius" : "Scorpio"); break;
+		case 12: Crowd[person].zodiac = (Crowd[person].birthday.day >= 22 ? "Capricorn" : "Sagittarius"); break;
+		default:Crowd[person].zodiac = "N/A"; break;
+		}
+	}
+	/*Bubble sort by birthday*/
+	for (int i = 0; i < N; i++)
+	{
+		for (int i = 0; i < N - 1; i++)
+		{
+			ZNAK temp;
+			if (Crowd[i].birthday.year > Crowd[i + 1].birthday.year)
+			{
+				temp = Crowd[i];
+				Crowd[i] = Crowd[i + 1];
+				Crowd[i + 1] = temp;
+			}
+			else if (Crowd[i].birthday.year == Crowd[i + 1].birthday.year)
+			{
+				if (Crowd[i].birthday.month > Crowd[i + 1].birthday.month)
+				{
+					temp = Crowd[i];
+					Crowd[i] = Crowd[i + 1];
+					Crowd[i + 1] = temp;
+				}
+				else if (Crowd[i].birthday.month == Crowd[i + 1].birthday.month)
+				{
+					if (Crowd[i].birthday.day > Crowd[i + 1].birthday.day)
+					{
+						temp = Crowd[i];
+						Crowd[i] = Crowd[i + 1];
+						Crowd[i + 1] = temp;
+					}
+				}
+			}
+		}
+	}
+
+	/*Array output*/
+	cout << "\n\nThe crew:\n";
+	for (int person = 0; person < N; person++)
+	{
+		printf("%-20s Sign: %-12s  Birthday: %02d.%02d.%d\n", &Crowd[person].name[0], &Crowd[person].zodiac[0], Crowd[person].birthday.day, Crowd[person].birthday.month, Crowd[person].birthday.year);
+	}
+
+	/*Sign entering*/
+	cout << "\nEnter the sign. Only those who share this sign will be displayed.\n";
+	string entering = "";
+	bool isMatch = false;
+	cin >> entering;
+
+	/*Checking if there's a match*/
+	for (int person = 0; person < N; person++)
+	{
+		/*If there is, print info about that person*/
+		if (_stricmp(&entering[0], &Crowd[person].zodiac[0]) == 0)
+		{
+			isMatch = true;
+			printf("%-20s Sign: %-12s  Birthday: %02d.%02d.%d\n", &Crowd[person].name[0], &Crowd[person].zodiac[0], Crowd[person].birthday.day, Crowd[person].birthday.month, Crowd[person].birthday.year);
+		}
+	}
+
+	/*If there isn't, print according message*/
+
+	if (!isMatch) 
+	{
+		cout << "No matches.";
+	}
+	cout << "\n\n";
 }
 
 int main()
