c6cfa504b90893017f22a15324695a32a3e262ca
Michael Potyomkin <54860902+BioAutomaton@users.noreply.github.com>
2020-03-15 15:49:46 +0200

    New solution for lab2.

--- TestingField/TestingField/TestingField.cpp
+++ TestingField/TestingField/TestingField.cpp
@@ -1,32 +1,20 @@
-﻿
-#include <conio.h>;
-#include <iostream>;
-
-using std::cin;
-using std::cout;
-using std::endl;
+﻿#include <iostream>
+using namespace std;
 
 int main()
 {
-    setlocale(LC_ALL, "Russian");
-    char Mother, Father, Sister, Brother;
-    cout << "Введите ФИО матери:\t";
-    cin >> Mother;
-    cout << "Введите ФИО отца:\t";
-    cin >> Father;
-    cout << "Введите ФИО сестры:\t";
-    cin >> Sister;
-    cout << "Введите ФИО брата:\t";
-    cin >> Brother;
-    cout << "\n";
-    cout << "\n";
-    cout << "Введённые данные:\n";
-    cout << "\n";
-    cout << "ФИО матери:\t" << Mother << endl;
-    cout << "ФИО отца:\t" << Father << endl;
-    cout << "ФИО сестры:\t" << Sister << endl;
-    cout << "ФИО брата:\t" << Brother;
+	string S;
+	cin >> S;
+	for (size_t i = 1; i < S.size(); i++)
+	{
+		if (S[i] == S[i - 1])
+		{
+			if (S[i] == 'z') { S[i] = 'a'; }
+			else {
+				S[i] += 1;
+			}
+		}
+	}
+	cout << S;
 
-    _getch();
-    return 0;
 }
