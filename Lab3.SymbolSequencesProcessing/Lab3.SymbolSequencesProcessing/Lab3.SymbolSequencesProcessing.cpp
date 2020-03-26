dd543bcd448470d5b84c78bff77307d753633ef8
Michael Potyomkin <54860902+BioAutomaton@users.noreply.github.com>
2020-03-26 23:59:15 +0200

    Lab 3 done. FINALLY!

---
+++ Lab3.SymbolSequencesProcessing/Lab3.SymbolSequencesProcessing/Lab3.SymbolSequencesProcessing.cpp
@@ -0,0 +1,164 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <string>
+#include <string.h>
+#include <windows.h>
+//#include <ctype.h>
+
+using namespace std;
+
+
+bool isLetter(char symbol)
+{
+	bool result = false;
+
+	if ((symbol >= 65 && symbol <= 90) || (symbol >= 97 && symbol <= 122))
+	{
+		result = true;
+	}
+	return result;
+}
+
+void BaseLevel()
+{
+	cout << "Enter 12 symbols:";
+	char entering[13];
+	cin.ignore(cin.rdbuf()->in_avail());
+	cin.getline(entering, sizeof(entering));
+
+	for (int i = 0; i < sizeof entering; i++)
+	{
+		/*ASCii table contains some strange symbols above (char)128 code. Therefore, many of entered symbols will be converted into gibberish */
+		entering[i] *= 2;
+	}
+
+	//char test[12];
+	//CharToOem(entering, test);
+	//cout << test << "\n";
+
+	cout << entering << "\n";
+}
+
+void MidLevel()
+{
+	cout << "Enter 10 symbols:";
+	char entering[11];
+	cin.ignore(cin.rdbuf()->in_avail());
+	cin.getline(entering, sizeof(entering));
+	_strlwr_s(entering);
+	for (int i = 1; i < sizeof entering; i++)
+	{
+		if (isLetter(entering[i]))
+		{
+			if (entering[i] == entering[i - 1])
+			{
+				cout << "letters " << entering[i - 1] << " and " << entering[i] << " (position " << (i) << ", " << (i + 1) << ") are similar.\n";
+			}
+		}
+	}
+}
+
+
+void HighLevel()
+{
+	wchar_t  entering[] = L"   тестируем.предложение.Testing sentence.";
+
+	char array[100] = "";
+	CharToOem(entering, array);
+
+	char temporary[200] = "";
+	int prvsCopiedSymbol = 0;
+	for (size_t i = 0; i < strlen(array); i++)
+	{
+		/*If punctuation is found*/
+		if (array[i] == '.' || array[i] == '!' || array[i] == '?')
+		{
+			if (array[i + 1] != ' ')
+			{
+				/*If there's no space after punctuation, copy this puntuation, then add space*/
+				strncat_s(temporary, &array[prvsCopiedSymbol], i - prvsCopiedSymbol + 1);
+				strcat_s(temporary, " ");
+				prvsCopiedSymbol = i + 1; //keep track of previous change
+			}
+		}
+		else
+		{
+			/*If current symbol in not a punctuation, copy it to temporary array and keep track of symbols*/
+			strncat_s(temporary, &array[prvsCopiedSymbol], i - prvsCopiedSymbol + 1);
+			prvsCopiedSymbol = i + 1;
+		}
+
+	}
+	for (int i = 0; i < strlen(temporary); i++)
+	{
+		if (temporary[i] == '.' || i == 0)
+		{
+			while (temporary[i] == ' ' || temporary[i] == '.')
+			{
+				i++;
+			}
+			if (temporary[i] >= 97 && temporary[i] <= 122)
+			{
+				temporary[i] -= (char)32;
+			}
+			else if (temporary[i] >= -96 && temporary[i] <= -81)
+			{
+				temporary[i] -= (char)32;
+			}
+			else if (temporary[i] >= -32 && temporary[i] <= -17)
+			{
+				temporary[i] -= (char)80;
+			}
+			else if (temporary[i] == -15)
+			{
+				temporary[i]--;
+			}
+		}
+
+	}
+
+	cout << temporary << endl;
+
+}
+
+int main()
+{
+	int select = 0;
+	do {
+		srand(time(NULL));
+		cout << "Variant 22. Lab 3.\nSelect difficulty level\n";
+		cout << "1 - Base level\n2 - Mid level \n3 - High level\n";
+		cout << "Or enter 0 to exit.\n";
+		bool isError = false;
+		string s;
+		do {
+			isError = false;
+			cin.ignore(cin.rdbuf()->in_avail());
+			cin.getline(&s[0], 10);
+			for (size_t i = 0; i < s.length(); i++)
+			{
+				if ((int)(s[i]) < 0 || !isdigit(s[i]))
+				{
+					isError = true;
+					break;
+				}
+			}
+			if (!isError)
+				select = atoi(s.c_str());
+			if (isError || select < 0 || select > 3)
+			{
+				cout << "Wrong entering.\n";
+			}
+		} while (isError || select < 0 || select > 3);
+
+		switch (select)
+		{
+		case 0: break;
+		case 1:BaseLevel(); break;
+		case 2:MidLevel(); break;
+		case 3:HighLevel(); break;
+		default: cout << "Wrong entering.\n"; break;
+		}
+	} while (select != 0);
+}
--- TestingField/TestingField/TestingField.cpp
+++ TestingField/TestingField/TestingField.cpp
@@ -1,187 +1,26 @@
-﻿#include<iostream>
-#include<string.h>
+﻿#include <iostream>
+#include <windows.h>
+
 using namespace std;
-
-struct name {
-    char F[56];
-    char I[32];
-    char O[32];
-    void setFullname() {
-        cout << "F: ";
-        cin.ignore(std::cin.rdbuf()->in_avail());
-        cin.getline(F, 56);
-
-        cout << endl << "I: ";
-        cin.ignore(std::cin.rdbuf()->in_avail());
-        cin.getline(I, 32);
-
-        cin.ignore(std::cin.rdbuf()->in_avail());
-        cout << endl << "o: ";
-        cin.getline(O, 32);
-    }
-    void showFullname() {
-        cout << "FIO: " << F << " " << I << " " << O << endl;
-    }
-};
-struct adress {
-    int index;
-    char country[30];
-    char region[30];
-    char district[30];
-    char city[20];
-    char street[50];
-    int house;
-    int apartment;
-    void setFulladress() {
-        cout << endl << "Index:";
-        while (!(cin >> index))
-        {
-            cout << "Wrong input" << endl;
-            cin.clear();
-            cin.ignore(65535, '\n');
-        }
-
-        cout << endl << "Country:";
-        cin.ignore(std::cin.rdbuf()->in_avail());
-        cin.getline(country, 30);
-
-        cout << endl << "Region:";
-        cin.ignore(std::cin.rdbuf()->in_avail());
-        cin.getline(region, 30);
-
-        cout << endl << "District:";
-        cin.ignore(std::cin.rdbuf()->in_avail());
-        cin.getline(district, 30);
-
-        cout << endl << "City:";
-        cin.ignore(std::cin.rdbuf()->in_avail());
-        cin.getline(city, 20);
-
-        cout << endl << "House:";
-        while (!(cin >> house))
-        {
-            cout << "Wrong input" << endl;
-            cin.clear();
-            cin.ignore(65535, '\n');
-        }
-
-        cout << endl << "Apartment:";
-        while (!(cin >> apartment))
-        {
-            cout << "Wrong input" << endl;
-            cin.clear();
-            cin.ignore(65535, '\n');
-        }
-    }
-    void showFulladress() {
-        cout << "Index:" << index << endl;
-        cout << "Country:" << country << endl;
-        cout << "Region:" << region << endl;
-        cout << "District:" << district << endl;
-        cout << "City:" << city << endl;
-        cout << "House:" << house << endl;
-        cout << "Apartment:" << apartment << endl;
-    }
-};
-struct owner {
-    name person;
-    long tele;
-    adress home;
-    char CarBrand[20];
-    int NumCar;
-    int NumTeh;
-    void setFullInfo() {
-        person.setFullname();
-
-        cout << endl << "Telephon number:";
-        cin >> tele;
-
-        home.setFulladress();
-
-        cout << endl << "Car brand:";
-        cin.ignore(std::cin.rdbuf()->in_avail());
-        cin.getline(CarBrand, 30);
-
-        cout << endl << "Car number:";
-        cin >> NumCar;
-
-        cout << endl << "Teh. passport number:";
-        cin >> NumTeh;
-
-    }
-    void outFullInfo() {
-        person.showFullname();
-
-        cout << "Telephon number:" << tele << endl;
-
-        home.showFulladress();
-
-        cout << "Car brand:" << CarBrand << endl;
-        cout << "Car number:" << NumCar << endl;
-        cout << "Teh. passport number:" << NumTeh << endl << endl;
-    }
-};
-
-
-void InitOfArray(owner* array, int* size)
-{
-    for (size_t i = 0; i < *size; i++)
-    {
-        (array + i)->setFullInfo();
-    }
-}
-
-void sortArray(owner* array, int* size)
-{
-
-    for (int i = 0; i < *size; i++)
-    {
-        for (int j = 0; j < *size - 1; j++) {
-            if (strcmp((array + j)->person.F, (array + j + 1)->person.F) < 0)
-            {
-                swap((array + j)->person.F, (array + j + 1)->person.F);
-            }
-        }
-    }
-}
-
-void showArray(owner* array, int* size)
-{
-    for (size_t i = 0; i < *size; i++)
-    {
-        (array + i)->outFullInfo();
-    }
-}
-
-void showbynumber(owner* array, int number)
-{
-    (array + number - 1)->outFullInfo();
-}
 
 int main()
 {
-    int size;
-    int number;
-    int YesOrNot = 0;
-    cout << "Enter size of array" << endl;
-    while (!(cin >> size))
+    wchar_t  S1[] = L"абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+    char lower[81];
+
+    wchar_t  S2[] = L"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+    char upper[81];
+
+    int a = CharToOem(S1, lower);
+    int b = CharToOem(S2, upper);
+    cout << lower << endl;
+    for (int i = 0; i < strlen(lower); i++)
     {
-        cout << "Wrong input" << endl;
-        cin.clear();
-        cin.ignore(65535, '\n');
+        cout << (int)lower[i] << " " << lower[i] << endl;
     }
-    int* ptrsize = &size;
-    owner* array = new owner[size];
-
-    InitOfArray(array, ptrsize);
-
-    sortArray(array, ptrsize);
-
-    showArray(array, ptrsize);
-
-    cout << "Enter number of owner" << endl;
-    cin >> number;
-    cout << "Owner " << number << ":" << endl;
-    showbynumber(array, number);
-
+    cout << upper << endl;
+    for (int i = 0; i < strlen(upper); i++)
+    {
+        cout << (int)upper[i] << " " << upper[i] << endl;
+    }
 }
